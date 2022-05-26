16b5cca8fda3a3cd24ea7264bcd72ba81a839576
EdvardKenua <orekhovsemyon@gmail.com>
2022-05-26 21:24:29 +0300

    Создан план класса

--- MathRegulations/Program.cs
+++ MathRegulations/Program.cs
@@ -6,57 +6,57 @@ namespace FromStringToMath // Note: actual namespace depends on the project name
 {
     internal class Program
     {   
-        public static Dictionary<string, string> elements = new Dictionary<string, string>();
-        static void Main(string[] args)
-        {
-            //string expresions = "";
-            //expresions = Console.ReadLine();
-            //Entity entity = expresions;
-            //Console.WriteLine(entity.Solve(""));
-            //string v = (entity.Solve("x").ToString());
+        //public static Dictionary<string, string> elements = new Dictionary<string, string>();
+        //static void Main(string[] args)
+        //{
+        //    //string expresions = "";
+        //    //expresions = Console.ReadLine();
+        //    //Entity entity = expresions;
+        //    //Console.WriteLine(entity.Solve(""));
+        //    //string v = (entity.Solve("x").ToString());
             
-            for (int i = 0; i < 2; i++)
-            {
-                elements.Add(Console.ReadLine(), null);
-            }
-            foreach (var item in elements)
-            {
-                Console.WriteLine($"Key: {item.Key}: {item.Value}");
-            }
-            for (int i = 0; i < elements.Count; i++)
-            {
-                string letter = Console.ReadLine();
-                elements[letter] = Console.ReadLine();
-            }
+        //    for (int i = 0; i < 2; i++)
+        //    {
+        //        elements.Add(Console.ReadLine(), null);
+        //    }
+        //    foreach (var item in elements)
+        //    {
+        //        Console.WriteLine($"Key: {item.Key}: {item.Value}");
+        //    }
+        //    for (int i = 0; i < elements.Count; i++)
+        //    {
+        //        string letter = Console.ReadLine();
+        //        elements[letter] = Console.ReadLine();
+        //    }
 
-            foreach (var item in elements)
-            {
-                Console.WriteLine($"Key: {item.Key}: {item.Value}");
-            }
-            Console.Write("Введите выражение:");
-            // x = a + b
-            string expres = Console.ReadLine();
-            Console.WriteLine(Changer(expres));
-            Entity entity = Changer(expres);
-            Console.WriteLine(entity.Solve("x").ToString());
+        //    foreach (var item in elements)
+        //    {
+        //        Console.WriteLine($"Key: {item.Key}: {item.Value}");
+        //    }
+        //    Console.Write("Введите выражение:");
+        //    // x = a + b
+        //    string expres = Console.ReadLine();
+        //    Console.WriteLine(Changer(expres));
+        //    Entity entity = Changer(expres);
+        //    Console.WriteLine(entity.Solve("x").ToString());
 
-        }
+        //}
 
-        public static string Changer(string rawExpres)
-        {
-            string newExpres = "";
-            for (int item = 0; item < rawExpres.Length; item++)
-            {
-                if (elements.ContainsKey(rawExpres[item].ToString()))
-                {
-                    newExpres += elements[rawExpres[item].ToString()];
-                    continue;
-                }
-                newExpres += rawExpres[item];
-            }
+        //public static string Changer(string rawExpres)
+        //{
+        //    string newExpres = "";
+        //    for (int item = 0; item < rawExpres.Length; item++)
+        //    {
+        //        if (elements.ContainsKey(rawExpres[item].ToString()))
+        //        {
+        //            newExpres += elements[rawExpres[item].ToString()];
+        //            continue;
+        //        }
+        //        newExpres += rawExpres[item];
+        //    }
 
-            return newExpres;
-        }
+        //    return newExpres;
+        //}
 
     }
 }
